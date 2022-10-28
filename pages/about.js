64e7651fd05cd99dae7d2dc5b8bac309da7ccf0a
aryanataandi <a.ryan.ata.ap@gmail.com>
2022-10-29 06:55:19 +0700

    about page init

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+export default function Archive() {
+  return (
+    <section className="my-36">
+      <h2 className="leading-none mb-8">
+        About Me<span className="text-purple-500">.</span>
+      </h2>
+      <div className="text-secondary">
+        <p className="text-xl mb-8">
+          My name is Aryanata Andipradana. You can call me Ryan. I love creating
+          digital products such as web application, design, animation, video and
+          more.
+        </p>
+        <p className="text-xl mb-8">
+          I work as a Software Engineer in a company that focused on web
+          application development. I build various web applications and provide
+          the best experience in solving real business problems. I handle both
+          back-end and front-end stack in my current company. My main focus
+          these days is building web app using good written code and learning
+          modern front-end technologies.
+        </p>
+        <p className="text-xl mb-8">
+          Besides of coding, I have interest in new tech stuff, gaming,
+          designing, photography, videography, meme, and exploring amazing
+          places.
+        </p>
+      </div>
+    </section>
+  );
+}
