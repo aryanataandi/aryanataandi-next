d90274a054e0c048a7483bc53bedcccc7ba62281
aryanataandi <a.ryan.ata.ap@gmail.com>
2022-11-04 07:26:48 +0700

    update 404 page

--- pages/404.js
+++ pages/404.js
@@ -5,11 +5,11 @@ export default function Custom404() {
     <section className="min-h-screen flex">
       <div className="m-auto text-center">
         <h1 className="text-6xl md:text-8xl mb-8">Oops!</h1>
-        <p className="text-xl md:text-body text-secondary mb-8">
-          Nothing here, are you lost? Need a hand? <br />- error code 404
+        <p className="text-xl md:text-body text-secondary mb-16">
+          404 - Page Not Found
         </p>
         <Link href="/">
-          <a className="btn-secondary">Go Home</a>
+          <a className="btn">Go Home</a>
         </Link>
       </div>
     </section>
